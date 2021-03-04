d90e2669ba516bcd1ded87bbf8a46818a281843f
dev <dev>
2021-03-04 10:28:50 +0400

    the delay module

--- Source/KAPDelay.cpp
+++ Source/KAPDelay.cpp
@@ -9,3 +9,32 @@
 */
 
 #include "KAPDelay.h"
+#include "JuceHeader.h"
+
+using namespace juce;
+
+KAPDelay::KAPDelay(): mSampleRate(-1){
+    
+}
+KAPDelay::~KAPDelay(){
+    
+}
+
+void KAPDelay::setSampleRate(double inSampleRate){
+    mSampleRate = inSampleRate;
+}
+
+void KAPDelay::reset(){
+    zeromem(mBuffer,  (sizeof(double)) * 2048);
+}
+
+void KAPDelay::process(float* inAudio,
+                       float inTime,
+                       float inFeedback,
+                       float inWetDry,
+                       float* outAudio,
+                       int inNumSamplesToRender){
+    const float wet = inWetDry;
+    const float dry = 1.0f - wet;
+    const float feedbackMapped = jmap(inFeedback, 0.0f, 1.0f, 0.0f, 0.95f);
+}
--- Source/KAPGain.cpp
+++ Source/KAPGain.cpp
@@ -9,6 +9,9 @@
 */
 
 #include "KAPGain.h"
+#include "JuceHeader.h"
+
+using namespace juce; // pas dans video
 
 KAPGain::KAPGain(){
     
@@ -23,7 +26,10 @@ void KAPGain::process(float* inAudio,
              float* outAudio,
                       int inNumSamplesToRender){
     
+    float gainMapped = jmap(inGain, 0.0f,1.0f, -24.0f, 24.0f);
+    gainMapped = Decibels::decibelsToGain(gainMapped, -24.0f);
+    
     for (int i = 0; i < inNumSamplesToRender; i++) {
-        outAudio[i] = inAudio[i] * inGain;
+        outAudio[i] = inAudio[i] * gainMapped;
     }
 }
