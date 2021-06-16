b5a9db63433baa53f8245c6af2d1e6d4ec57d101
coursju <coursju@outlook.com>
2021-06-16 20:16:10 +0400

    Look and Feel Class & Making Buttons

--- Source/KAPLookAndFeel.h
+++ Source/KAPLookAndFeel.h
@@ -9,3 +9,53 @@
 */
 
 #pragma once
+
+#include "JuceHeader.h"
+#include "KAPInterfaceDefines.h"
+
+class KAPLookAndFeel:
+    public LookAndFeel_V4
+{
+public:
+    KAPLookAndFeel()
+    {
+        // combox colours
+        setColour(ComboBox::backgroundColourId, KAPColour_3);
+        setColour(ComboBox::outlineColourId, KAPColour_2);
+        setColour(ComboBox::arrowColourId, KAPColour_1);
+        setColour(ComboBox::textColourId, KAPColour_1);
+
+        // button text colours
+        setColour(TextButton::buttonColourId, KAPColour_1);
+        setColour(TextButton::textColourOnId, KAPColour_1);
+        setColour(TextButton::textColourOffId, KAPColour_1);
+
+    }
+    
+    ~KAPLookAndFeel();
+    
+    Font getTextButtonFont (TextButton&, int buttonHeight) override
+    {
+        return font_1;
+    }
+    
+    void drawButtonBackground (Graphics& g, Button& button, const Colour& backgroundColour,
+                               bool isMouseOverButton, bool isButtonDown) override
+    {
+        Colour fillColour;
+        
+        if (isButtonDown) {
+            fillColour = KAPColour_5;
+        } else if(isMouseOverButton){
+            fillColour = KAPColour_3;
+        }
+        
+        const float cornersize = 6.0f;
+        const juce::Rectangle<float> bounds = button.getLocalBounds().toFloat().reduced(0.5f, 0.5f);
+        
+        g.setColour(fillColour);
+        g.fillRoundedRectangle(bounds.reduced(1), cornersize);
+    }
+    
+private:
+};
