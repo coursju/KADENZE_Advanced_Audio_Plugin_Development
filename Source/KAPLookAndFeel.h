e994af5a2b211ca9bc8f14365ea546aa62262a68
coursju <coursju@outlook.com>
2021-06-17 19:12:39 +0400

    Styling Combo Boxes Part 1

--- Source/KAPLookAndFeel.h
+++ Source/KAPLookAndFeel.h
@@ -34,6 +34,7 @@ public:
     
     ~KAPLookAndFeel();
     
+    /** buttons */
     Font getTextButtonFont (TextButton&, int buttonHeight) override
     {
         return font_1;
@@ -56,6 +57,54 @@ public:
         g.setColour(fillColour);
         g.fillRoundedRectangle(bounds.reduced(1), cornersize);
     }
+    
+    /** comboboxes */
+    
+    void drawPopupMenuItem (Graphics& g, const Rectangle<int>& area,
+                            bool isSeparator, bool isActive, bool isHighlighted, bool isTicked, bool hasSubMenu,
+                            const String& text, const String& shortcutKeyText,
+                            const Drawable* icon, const Colour* textColour) override
+    {
+        juce::Rectangle<int> r (area);
+        
+        Colour fillColour = isHighlighted ? KAPColour_5 : KAPColour_4;
+        
+        g.setColour(fillColour);
+        g.fillRect( r.getX(), r.getY(), r.getWidth(), r.getHeight() -1);
+        
+        Colour myTextColour = isTicked ? KAPColour_7 : KAPColour_1;
+        g.setColour( myTextColour);
+        g.setFont(font_1);
+        
+        r.setLeft(10);
+        r.setY(1);
+        g.drawFittedText(text, r, Justification::left, 1);
+    }
+    
+    void drawComboBox (Graphics& g, int width, int height, bool isButtonDown,
+                       int buttonX, int buttonY, int buttonW, int buttonH,
+                       ComboBox& box) override
+    {
+        const float cornersize = 3.0f;
+        const juce::Rectangle<int> boxBounds (0, 0, width, height);
+        
+        g.setColour(KAPColour_3);
+        g.fillRoundedRectangle(boxBounds.toFloat(), cornersize);
+        
+        juce::Rectangle<int> arrow (width - 30, 0, 20, height);
+        
+        Path path;
+        
+        path.startNewSubPath(arrow.getX() + 3.0f, arrow.getCentreY() - 2.0f);
+        path.lineTo(arrow.toFloat().getCentreX(), arrow.toFloat().getCentreY() - 2.0f);
+        path.lineTo(arrow.toFloat().getRight(), arrow.toFloat().getCentreY() - 2.0f);
+        
+        Colour arrowColour = box.findColour(ComboBox::arrowColourId).withAlpha(box.isEnabled() ? 0.9f : 0.2f);
+        g.setColour(arrowColour);
+        g.strokePath(path, PathStrokeType(2.0f));
+    }
+    
+    /** sliders */
     
 private:
 };
