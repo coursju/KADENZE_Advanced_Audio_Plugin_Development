8afc7623e093592f4b92ec56eb5118bfade004ed
coursju <coursju@outlook.com>
2021-06-19 10:14:03 +0400

    Adding Image Assets: Knobs

--- Source/KAPLookAndFeel.h
+++ Source/KAPLookAndFeel.h
@@ -19,6 +19,9 @@ class KAPLookAndFeel
 public:
     KAPLookAndFeel()
     {
+        // store image assets
+        mSliderImage = ImageCache::getFromMemory(BinaryData::kadenze_knob_png, BinaryData::kadenze_knob_pngSize);
+        
         // combox colours
         setColour(ComboBox::backgroundColourId, KAPColour_3);
         setColour(ComboBox::outlineColourId, KAPColour_2);
@@ -106,6 +109,32 @@ public:
     
     /** sliders */
     
+    void drawRotarySlider (Graphics& g, int x, int y, int width, int height,
+                           float sliderPosProportional, float rotaryStartAngle,
+                           float rotaryEndAngle, Slider& slider) override
+    {
+        const int numFrames = mSliderImage.getHeight() / mSliderImage.getWidth();
+        const int frameIndex = (int)std::ceil(sliderPosProportional * (numFrames - 1));
+        
+        const float radius = jmin(width*0.5, height*0.5);
+        const float centreX = x + width * 0.5f;
+        const float centreY = y + height * 0.5f;
+        const float rx = centreX - radius;
+        const float ry = centreY - radius;
+        
+        g.drawImage(mSliderImage,
+                    rx,
+                    ry,
+                    2*radius,
+                    2*radius,
+                    0,
+                    frameIndex * mSliderImage.getWidth(),
+                    mSliderImage.getWidth(),
+                    mSliderImage.getWidth());
+    }
+    
 private:
+    
+    Image mSliderImage;
     
 };
